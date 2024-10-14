d62a361534f06854e18a0e3a0915a5e62b472129
SingularityV3 <lukehulan@gmail.com>
2024-10-14 08:54:55 -0400

    Add first representation functions

--- interpreter.c
+++ interpreter.c
@@ -1,3 +1,7 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+
 /*
 * Number data type. Uses a union to handle both integers and doubles.
 * 
@@ -32,12 +36,12 @@ typedef struct multipleStatements_s {
     union {
         struct {
             struct statement_s* stmnt;
-        } SINGLE;
+        } single_t;
 
         struct {
             struct statement_s* first;
             struct multipleStatements_s* rest;
-        } MULTI;
+        } multi_t;
     };
 } multipleStatements_s;
 
@@ -48,34 +52,34 @@ typedef struct statement_s {
     union {
         struct {
             struct multipleStatements_s* stmnts;
-        } BLOCK;
+        } block_t;
 
         struct {
             struct bindingIdentifier_s* bid;
             struct expression_s* expr;
-        } DEFINE;
+        } define_t;
 
         struct {
             struct identifier_s* id;
             struct list_s* list;
-        } EXECUTE;
+        } execute_t;
 
         struct {
             struct expression_s* expr;
-        } RETURN;
+        } return_t;
 
         struct {
             struct if_s* stmnt;
-        } IF;
+        } if_t;
 
         struct {
             struct expression_s* expr;
             struct statement_s* stmnt;
-        } WHILE;
+        } while_t;
 
         struct {
             int placeholder;
-        } PASS;
+        } pass_t;
     };
 } statement_s;
 
@@ -87,26 +91,26 @@ typedef struct if_s {
         struct {
             struct expression_s* ifExpr;
             struct statement_s* ifStmnt;
-        } IF_ONLY;
+        } if_only_t;
 
         struct {
             struct expression_s* ifExpr;
             struct statement_s* ifStmnt;
             struct statement_s* elseStmnt;
-        } IF_ELSE;
+        } if_else_t;
 
         struct {
             struct expression_s* ifExpr;
             struct statement_s* ifStmnt;
             struct elseIfChain_s* chain;
-        } IF_ELSEIF;
+        } if_elseif_t;
 
         struct {
             struct expression_s* ifExpr;
             struct statement_s* ifStmnt;
             struct elseIfChain_s* chain;
             struct statement_s* elseStmnt;
-        } IF_ELSEIF_ELSE;
+        } if_elseif_else_t;
     };
 } if_s;
 
@@ -116,13 +120,13 @@ typedef struct elseIfChain_s {
         struct {
             struct expression_s* ifExpr;
             struct statement_s* ifStmnt;
-        } SINGLE;
+        } single_t;
 
         struct {
             struct expression_s* ifExpr;
             struct statement_s* ifStmnt;
             struct elseIfChain_s* chain;
-        } MULTI;
+        } multi_t;
     };
 } elseIfChain_s;
 
@@ -133,36 +137,36 @@ typedef struct expression_s {
     union {
         struct {
             struct expression_s* expr;
-        } PARENTHESES;
+        } parentheses_t;
 
         struct {
             struct identifier_s* id;
-        } IDENTIFIER;
+        } identifier_t;
 
         struct {
             struct numeric_s* num;
-        } NUMERIC;
+        } numeric_t;
 
         struct {
             struct boolean_s* bool;
-        } BOOLEAN;
+        } boolean_t;
 
         struct {
             struct character_s* chr;
-        } CHARACTER;
+        } character_t;
 
         struct {
             struct list_s* list;
-        } LIST;
+        } list_t;
 
         struct {
             struct identifier_s* id;
             struct list_s* list;
-        } EVALUATE;
+        } evaluate_t;
 
         struct {
             struct functionDeclaration_s* func;
-        } FUNCTION;
+        } function_t;
     };
 } expression_s;
 
@@ -174,13 +178,13 @@ typedef struct numeric_s {
     union {
         struct {
             struct numberData_s* num;
-        } NUMBER;
+        } number_t;
 
         struct {
             struct numberData_s* num1;
             struct numberData_s* num2;
             enum operator op;
-        } NUMERIC_OPERATOR;
+        } numeric_operator_t;
     }; 
 } numeric_s;
 
@@ -191,30 +195,30 @@ typedef struct boolean_s {
     union {
         struct {
             int placeholder;
-        } TRUE;
+        } true_t;
 
         struct {
             int placeholder;
-        } FALSE;
+        } false_t;
 
         struct {
             struct expression_s* expr;
-        } NOT;
+        } not_t;
 
         struct {
             struct expression_s* expr1;
             struct expression_s* expr2;
-        } OR;
+        } or_t;
 
         struct {
             struct expression_s* expr1;
             struct expression_s* expr2;
-        } AND;
+        } and_t;
 
         struct {
             struct expression_s* expr1;
             struct expression_s* expr2;
-        } EQUALS;
+        } equals_t;
     };
 } boolean_s;
 
@@ -225,11 +229,11 @@ typedef struct list_s {
     union {
         struct {
             int placeholder;
-        } EMPTY;
+        } empty_t;
 
         struct {
             struct multipleExpressions_s* exprs;
-        } NOT_EMPTY;
+        } not_empty_t;
     };
 } list_s;
 
@@ -238,12 +242,12 @@ typedef struct multipleExpressions_s {
     union {
         struct {
             struct expression_s* expr;
-        } SINGLE;
+        } single_t;
 
         struct {
             struct expression_s* first;
             struct multipleExpressions_s* rest;
-        } MULTI;
+        } multi_t;
     };
 } multipleExpressions_s;
 
@@ -255,13 +259,13 @@ typedef struct functionDeclaration_s {
         struct {
             struct list_s* list;
             struct statement_s* stmnt;
-        } NOT_TYPED;
+        } not_typed_t;
 
         struct {
             struct identifier_s* id;
             struct list_s* list;
             struct statement_s* stmnt;
-        } TYPED;
+        } typed_t;
     };
 } functionDeclaration_s;
 
@@ -270,12 +274,12 @@ typedef struct bindingIdentifier_s {
     union {
         struct {
             struct identifier_s* id;
-        } NOT_TYPED;
+        } not_typed_t;
 
         struct {
             struct identifier_s* type;
             struct identifier_s* id;
-        } TYPED;
+        } typed_t;
     };
 } bindingIdentifier_s;
 
@@ -287,51 +291,217 @@ typedef struct identifier_s {
     char* identifier;
 } identifier_s;
 
+// Forward declarations for representation functions
+char* numberDataRepr(numberData_s* param);
+char* programRepr(program_s* param);
+char* multipleStatementsRepr(multipleStatements_s* param);
+char* statementRepr(statement_s* param);
+char* ifRepr(if_s* param);
+char* elseIfChainRepr(elseIfChain_s* param);
+char* expressionRepr(expression_s* param);
+char* numericRepr(numeric_s* param);
+char* booleanRepr(boolean_s* param);
+char* listRepr(list_s* param);
+char* multipleExpressionsRepr(multipleExpressions_s* param);
+char* functionDeclarationRepr(functionDeclaration_s* param);
+char* bindingIdentifierRepr(bindingIdentifier_s* param);
+char* characterRepr(character_s* param);
+char* identifierRepr(identifier_s* param);
+
+// Representation functions
+char* numberDataRepr(numberData_s* param) {
+    int paramlength;
+    char* paramStr;
+    int returnLength;
+    char* returnStr;
+
+    if (param->type == INT) {
+        paramlength = snprintf(NULL, 0, "%d", param->integer) + 1;
+        paramStr = malloc(paramlength);
+        snprintf(paramStr, paramlength, "%d", param->integer);
+    } else if (param->type == DOUBLE) {
+        paramlength = snprintf(NULL, 0, "%lf", param->decimal) + 1;
+        paramStr = malloc(paramlength);
+        snprintf(paramStr, paramlength, "%lf", param->decimal);
+    } else {
+        return "Error: invalid or unset tag.";
+    }
+
+    returnLength = strlen("NumberData: []") + paramlength;
+    returnStr = malloc(returnLength);
+
+    snprintf(returnStr, returnLength, "%s%s%s", "NumberData: [", paramStr, "]");
+    return returnStr;
+}
+
+char* programRepr(program_s* param) {
+    char* multStmntStr = multipleStatementsRepr(param->stmnts);
+    int returnLength = strlen("Program: []") + strlen(multStmntStr) + 1;
+    char* returnStr = malloc(returnLength);
+
+    snprintf(returnStr, returnLength, "%s%s%s", "Program: [", multStmntStr, "]");
+    return returnStr;
+}
+
+char* multipleStatementsRepr(multipleStatements_s* param) {
+    char* tagName;
+    int returnLength;
+    char* returnStr;
+
+    if (param->tag == SINGLE) {
+        tagName = "single";
+
+        char* stmntStr = statementRepr(param->single_t.stmnt);
+
+        returnLength = strlen("MultipleStatements(): []") + strlen(tagName) + strlen(stmntStr) + 1;
+        returnStr = malloc(returnLength);
+
+        snprintf(returnStr, returnLength, "%s%s%s%s%s", "MultipleStatements(", tagName, "): [", stmntStr, "]");
+    } else if (param->tag == MULTI) {
+        tagName = "multi";
+
+        char* firstStr = statementRepr(param->multi_t.first);
+        char* restStr = multipleStatementsRepr(param->multi_t.rest);
+        
+        returnLength = strlen("MultipleStatements(): [, ]") + strlen(tagName) + strlen(firstStr) + strlen(restStr) + 1;
+        returnStr = malloc(returnLength);
+
+        snprintf(returnStr, returnLength, "%s%s%s%s%s%s%s", "MultipleStatements(", tagName, "): [", firstStr, ", ", restStr, "]");
+    } else {
+        return "Error: invalid or unset tag.";
+    }
+
+    return returnStr;
+}
+
+char* statementRepr(statement_s* param) {
+    char* tagName;
+    int returnLength;
+    char* returnStr;
+
+    if (param->tag == BLOCK) {
+        tagName = "block";
+
+        char* multStmntStr = multipleStatementsRepr(param->block_t.stmnts);
+
+        returnLength = strlen("Statement(): []") + strlen(tagName) + strlen(multStmntStr) + 1;
+        returnStr = malloc(returnLength);
+
+        snprintf(returnStr, returnLength, "%s%s%s%s%s", "Statement(", tagName, "): [", multStmntStr, "]");
+    } else if (param->tag == DEFINE) {
+        tagName = "define";
+
+        char* bidStr = bindingIdentifierRepr(param->define_t.bid);
+        char* exprStr = expressionRepr(param->define_t.expr);
+
+        returnLength = strlen("Statement(): []") + strlen(tagName) + strlen(bidStr) + strlen(exprStr) + 1;
+        returnStr = malloc(returnLength);
+
+        snprintf(returnStr, returnLength, "%s%s%s%s%s%s%s", "Statement(", tagName, "): [", bidStr, ", ", exprStr, "]");
+    } else if (param->tag == EXECUTE) {
+        tagName = "execute";
+
+        char* idStr = identifierRepr(param->execute_t.id);
+        char* listStr = listRepr(param->execute_t.list);
+
+        returnLength = strlen("Statement(): []") + strlen(tagName) + strlen(idStr) + strlen(listStr) + 1;
+        returnStr = malloc(returnLength);
+
+        snprintf(returnStr, returnLength, "%s%s%s%s%s%s%s", "Statement(", tagName, "): [", idStr, ", ", listStr, "]");
+    } else if (param->tag == RETURN) {
+        tagName = "return";
+
+        char* exprStr = expressionRepr(param->return_t.expr);
+
+        returnLength = strlen("Statement(): []") + strlen(tagName) + strlen(exprStr) + 1;
+        returnStr = malloc(returnLength);
+
+        snprintf(returnStr, returnLength, "%s%s%s%s%s", "Statement(", tagName, "): [", exprStr, "]");
+    } else if (param->tag == IF) {
+        tagName = "if";
+
+        char* ifStr = ifRepr(param->if_t.stmnt);
+
+        returnLength = strlen("Statement(): []") + strlen(tagName) + strlen(ifStr) + 1;
+        returnStr = malloc(returnLength);
+
+        snprintf(returnStr, returnLength, "%s%s%s%s%s", "Statement(", tagName, "): [", ifStr, "]");
+    } else if (param->tag == WHILE) {
+        tagName = "while";
+        
+        char* exprStr = expressionRepr(param->while_t.expr);
+        char* stmntStr = statementRepr(param->while_t.stmnt);
+
+        returnLength = strlen("Statement(): []") + strlen(tagName) + strlen(exprStr) + strlen(stmntStr) + 1;
+        returnStr = malloc(returnLength);
+
+        snprintf(returnStr, returnLength, "%s%s%s%s%s%s%s", "Statement(", tagName, "): [", exprStr, ", ", stmntStr, "]");
+    } else if (param->tag == PASS) {
+        tagName = "pass";
+        returnLength = strlen("Statement()") + strlen(tagName) + 1;
+        returnStr = malloc(returnLength);
+
+        snprintf(returnStr, returnLength, "%s", "Statement(", tagName, ")");
+    } else {
+        return "Error: invalid or unset tag.";
+    }
+
+    return returnStr;
+}
+
 int main() {
     // "define x: 5"
-    identifier_s defStmntId;
-    defStmntId.identifier = "x";
-    bindingIdentifier_s defStmntBid;
-    defStmntBid.tag = NOT_TYPED;
-    defStmntBid.NOT_TYPED.id = &defStmntId;
-    numberData_s fiveInt;
-    fiveInt.type = INT;
-    fiveInt.integer = 5;
-    numeric_s numFive;
-    numFive.tag = NUMBER;
-    numFive.NUMBER.num = &fiveInt;
-    expression_s numExpr;
-    numExpr.tag = NUMERIC;
-    numExpr.NUMERIC.num = &numFive;
-    statement_s defStmnt;
-    defStmnt.tag = DEFINE;
-    defStmnt.DEFINE.bid = &defStmntBid;
-    defStmnt.DEFINE.expr = &numExpr;
-    multipleStatements_s defMultStmnt;
-    defMultStmnt.tag = SINGLE;
-    defMultStmnt.SINGLE.stmnt = &defStmnt;
-    program_s defStmntRoot;
-    defStmntRoot.stmnts = &defMultStmnt;
+    identifier_s* defStmntId = malloc(sizeof(identifier_s));
+    defStmntId->identifier = "x";
+    bindingIdentifier_s* defStmntBid = malloc(sizeof(bindingIdentifier_s));
+    defStmntBid->tag = NOT_TYPED;
+    defStmntBid->not_typed_t.id = defStmntId;
+    numberData_s* fiveInt = malloc(sizeof(numberData_s));
+    fiveInt->type = INT;
+    fiveInt->integer = 5;
+    numeric_s* numFive = malloc(sizeof(numeric_s));
+    numFive->tag = NUMBER;
+    numFive->number_t.num = fiveInt;
+    expression_s* numExpr = malloc(sizeof(expression_s));
+    numExpr->tag = NUMERIC;
+    numExpr->numeric_t.num = numFive;
+    statement_s* defStmnt = malloc(sizeof(statement_s));
+    defStmnt->tag = DEFINE;
+    defStmnt->define_t.bid = defStmntBid;
+    defStmnt->define_t.expr = numExpr;
+    multipleStatements_s* defMultStmnt = malloc(sizeof(multipleStatements_s));
+    defMultStmnt->tag = SINGLE;
+    defMultStmnt->single_t.stmnt = defStmnt;
+    program_s* defStmntRoot = malloc(sizeof(program_s));
+    defStmntRoot->stmnts = defMultStmnt;
 
     // "if true: def x: 5"
-    boolean_s boolTrue;
-    boolTrue.tag = TRUE;
-    boolTrue.TRUE.placeholder = 1;
-    expression_s boolExpr;
-    boolExpr.tag = BOOLEAN;
-    boolExpr.BOOLEAN.bool = &boolTrue;
-    if_s ifCond;
-    ifCond.tag = IF_ONLY;
-    ifCond.IF_ONLY.ifExpr = &boolExpr;
-    ifCond.IF_ONLY.ifStmnt = &defStmnt;
-    statement_s ifStmnt;
-    ifStmnt.tag = IF;
-    ifStmnt.IF.stmnt = &ifCond;
-    multipleStatements_s ifMultStmnt;
-    ifMultStmnt.tag = SINGLE;
-    ifMultStmnt.SINGLE.stmnt = &ifStmnt;
-    program_s ifStmntRoot;
-    ifStmntRoot.stmnts = &ifMultStmnt;
+    boolean_s* boolTrue = malloc(sizeof(boolean_s));
+    boolTrue->tag = TRUE;
+    boolTrue->true_t.placeholder = 1;
+    expression_s* boolExpr = malloc(sizeof(expression_s));
+    boolExpr->tag = BOOLEAN;
+    boolExpr->boolean_t.bool = boolTrue;
+    if_s* ifCond = malloc(sizeof(if_s));
+    ifCond->tag = IF_ONLY;
+    ifCond->if_only_t.ifExpr = boolExpr;
+    ifCond->if_only_t.ifStmnt = defStmnt;
+    statement_s* ifStmnt = malloc(sizeof(statement_s));
+    ifStmnt->tag = IF;
+    ifStmnt->if_t.stmnt = ifCond;
+    multipleStatements_s* ifMultStmnt = malloc(sizeof(multipleStatements_s));
+    ifMultStmnt->tag = SINGLE;
+    ifMultStmnt->single_t.stmnt = ifStmnt;
+    program_s* ifStmntRoot = malloc(sizeof(program_s));
+    ifStmntRoot->stmnts = ifMultStmnt;
+
+    printf("%s", numberDataRepr(fiveInt));
+
+    statement_s* passStmnt = malloc(sizeof(statement_s));
+    passStmnt->tag = PASS;
+    passStmnt->pass_t.placeholder = 1;
+
+    printf("%s", statementRepr(passStmnt));
 
     return 0;
 }
